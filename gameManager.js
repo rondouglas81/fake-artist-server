9f4db6d07a84edc93ea87057dbc9cc328bbf2fb4
rondouglas <rondouglas@gmail.com>
2020-04-27 01:14:49 -0600

    current player

--- gameManager.js
+++ gameManager.js
@@ -3,7 +3,9 @@ const _ = require('lodash');
 
 var _io, _socket;
 
-const gamesList = [];
+const gamesList = {};
+
+const colors = ['red', 'blue', 'green', 'purple', 'orange'];
 
 exports.init = function (io, socket) {
   _io = io;
@@ -12,25 +14,35 @@ exports.init = function (io, socket) {
   _socket.emit('connected', { message: 'You are connected!' });
 
   _socket.on('hostGame', onHostGame);
-  _socket.on('hostGameStart', onHostGameStart);
-  _socket.on('hostGameEnd', onHostGameEnd);
+  _socket.on('hostStartGame', onHostGameStart);
+  _socket.on('hostEndGame', onHostGameEnd);
 
   _socket.on('joinGame', onJoinGame);
+  _socket.on('playerDrawFinished', onPlayerDrawFinished);
 
   _socket.on('message', onMessage);
 };
 
 exports.createGame = function () {
   const gameId = randomId();
-  gamesList.push({ gameId, players: [], state: 'notStarted' });
+  gamesList[gameId] = {
+    gameId,
+    hostId: null,
+    players: [],
+    state: 'notStarted',
+    round: 0,
+    lineData: [],
+    currentPlayerIndex: 0,
+  };
   console.log(gamesList);
   return gameId;
 };
 
 function onHostGame(params) {
-  const game = _.find(gamesList, ['gameId', params.gameId]);
+  const game = gamesList[params.gameId];
   if (game) {
     console.log('host joined game', params.gameId);
+    game.hostId = this.id;
     this.join(params.gameId);
     this.emit('hostJoinedGame', game);
   } else {
@@ -39,29 +51,83 @@ function onHostGame(params) {
   }
 }
 
-function onHostGameStart(params) {}
+function onHostGameStart() {
+  const game = _.find(gamesList, ['hostId', this.id]);
+  if (game) {
+    console.log('found the game for this host', game.gameId);
+    game.state = 'started';
+    game.players = _.shuffle(game.players);
+    game.players = game.players.map((player, index) => ({
+      ...player,
+      color: colors[index],
+    }));
+    game.currentPlayerIndex = 0;
+    _io.to(game.gameId).emit('gameStarted', { gameData: game });
+  }
+}
 
-function onHostGameEnd(params) {}
+function onHostGameEnd() {
+  const game = _.find(gamesList, ['hostId', this.id]);
+  if (game) {
+    console.log('found the game for this host', game.gameId);
+    game.state = 'ended';
+    _io.to(game.gameId).emit('gameEnded', { gameData: game });
+  }
+}
 
 function onJoinGame(params) {
-  const game = _.find(gamesList, ['gameId', params.gameId]);
+  const game = gamesList[params.gameId];
 
   if (game) {
-    if (_.includes(game.players, params.username)) {
+    const player = _.find(game.players, ['name', params.username]);
+    if (player) {
       console.log('player reconnected', params.gameId, params.username);
+      player.id = this.id;
       this.join(params.gameId);
       _io.to(game.gameId).emit('playerReconnected', {
         username: params.username,
         gameData: game,
       });
-    } else {
+    } else if (game.state === 'notStarted') {
       console.log('player joined game', params.gameId, params.username);
-      game.players.push(params.username);
+      game.players.push({ id: this.id, name: params.username });
       this.join(params.gameId);
       _io.to(game.gameId).emit('playerJoinedGame', {
         username: params.username,
         gameData: game,
       });
+    } else {
+      console.log('game', params.gameId, 'already started');
+      this.emit('alreadyStarted', params.gameId);
+    }
+  } else {
+    console.log('game', params.gameId, 'not found');
+    this.emit('notFound', params.gameId);
+  }
+}
+
+function onPlayerDrawFinished(params) {
+  console.log('player finished drawing', this.id);
+
+  const game = gamesList[params.gameId];
+  if (game) {
+    if (this.id === game.players[game.currentPlayerIndex].id) {
+      game.currentPlayerIndex++;
+      if (game.currentPlayerIndex >= game.players.length) {
+        game.currentPlayerIndex = 0;
+        game.round++;
+        if (game.round > 1) {
+          game.state = 'drawingEnded';
+        }
+      }
+      _io.to(game.gameId).emit('gameUpdated', { gameData: game });
+    } else {
+      console.log(
+        'its not this players turn',
+        this.id,
+        game.players[game.currentPlayerIndex].id
+      );
+      this.emit('notYourTurn');
     }
   } else {
     console.log('game', params.gameId, 'not found');
